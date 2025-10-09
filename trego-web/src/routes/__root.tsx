5d2f653df8793eccd97e1b406f73164563837fed
Alakh Chandarana <36898190+alakhpc@users.noreply.github.com>
2025-10-09 16:45:54 -0400

    Fix devtools

--- trego-web/src/routes/__root.tsx
+++ trego-web/src/routes/__root.tsx
@@ -1,22 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
+      <TanStackRouterDevtools />
     </>
   ),
 });
